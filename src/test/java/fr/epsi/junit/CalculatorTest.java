5f511d8e68ee48594b656fdcf67cfb376efbe32a
sczMaster <steven.sacaze@epsi.fr>
2021-06-23 11:35:06 +0200

    updates

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -20,15 +20,41 @@ public class CalculatorTest {
        // TODO
     	int a = 5;
         int b = 3;
-        int result = a+b;
-        Assertions.assertEquals(result, Calculator.add(a, b));
+        int result = calculator.add(a,b);;
+        Assertions.assertEquals(8, result);
     }
     
     @Test
     public void should_substract_two_numbers(){
         // TODO
-    	    	
+    	int a = 6;
+        int b = 4;
+        int result = calculator.substract(a, b);
+        Assertions.assertEquals(2, result); 	
     	
+    }
+    
+    @Test
+    public void should_multi_two_numbers(){
+    	int a = 7;
+        int b = 5;
+        int result = calculator.multi(a, b);
+        Assertions.assertEquals(40, result); 
+    }
+    
+    @Test
+    public void should_div_two_numbers(){
+    	int a = 1;
+        int b = 4;
+        int result = calculator.div(a, b);
+        Assertions.assertEquals(0.25, result); 
+    }
+
+    @Test
+    public void should_add_tab(){
+        int nmbers[] = {2,2,4,2,5};
+        int result = calculator.add(nmbers);
+        Assertions.assertEquals(15, result);
     }
 
 
